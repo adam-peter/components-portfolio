4acb1cc62a66042b8fd6e4059df69398aba39875
badmood111 <petrzela.adam@seznam.cz>
2023-04-03 18:05:37 +0200

    SimpleNavigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-import SimpleCard from "@/components/1_simple_card";
-
 import styles from "./Home.module.scss";
+
+import SimpleCard from "@/components/1_simple_card";
+import SimpleNavigation from "@/components/2_simple_navigation";
 
 const Home = () => {
   return (
     <div className={styles.home}>
       <SimpleCard />
+      
     </div>
   );
 };
--- components/2_simple_navigation/index.tsx
+++ components/2_simple_navigation/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
-const index = () => {
+import { FaSearch } from "react-icons/fa";
+
+import styles from "./styles.module.scss";
+
+const links = ["Home", "Services", "About us", "FAQ", "Contact us"];
+
+const SimpleNavigation = () => {
+  //This is a scuffed component I don't want to finish.
+
   return (
-    <div>
-      
+    <div className={styles.nav}>
+      <div className={styles.searchBar}>
+        <FaSearch className={styles.searchIcon}/>
+        <input className={styles.searchInput} />
+      </div>
+      <div className={styles.links}>
+        {links.map((link: String) => (
+          <Link
+            href="#"
+            className={styles.link}
+            key={links.indexOf(link.toString())}
+          >
+            {link}
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default SimpleNavigation;
